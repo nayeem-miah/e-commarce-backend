097dc4e8ec7c511472242331446cf22866cea22c
nayeem-miah <nayeem5113a@gmail.com>
2025-08-22 21:16:21 +0600

    product update and get all product success

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -4,8 +4,6 @@ import { Product } from "./product.model";
 const addProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const result = await Product.create(req.body);
-        console.log(result);
-
 
         res.status(201).json({
             success: true,
@@ -17,8 +15,45 @@ const addProduct = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+const getProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await Product.find();
+
+        res.status(201).json({
+            success: true,
+            message: "all product received success",
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const updateProduct = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = req.params.id
+        const updateData = req.body;
+
+        const result = await Product.findByIdAndUpdate(
+            id,
+            updateData,
+            { new: true, runValidators: true }
+        );
+
+        res.status(201).json({
+            success: true,
+            message: "product updated success success",
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 
 
 export const ProductController = {
-    addProduct
+    addProduct,
+    getProducts,
+    updateProduct
 }
--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import { ProductController } from "./product.controller";
 import { validationRequest } from "../../middlewares/validationRequest";
-import { createProductSchema } from "./product.validation";
+import { createProductSchema, updateProductSchema } from "./product.validation";
 
 const router = Router();
 
 router.post("/create",
     validationRequest(createProductSchema),
     ProductController.addProduct
+);
+
+router.get("/", ProductController.getProducts);
+
+router.patch("/:id",
+    validationRequest(updateProductSchema),
+    ProductController.updateProduct
 )
 
 export const ProductRouter = router;
